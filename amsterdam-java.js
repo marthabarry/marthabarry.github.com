8b720847d412a6d0d39f5c4f9527fb58c0817c3c
Martha <mrth.bry@gmail.com>
2013-04-03 01:33:09 -0400

    ITS FINISHED

--- amsterdam-java.js
+++ amsterdam-java.js
@@ -1,10 +1,164 @@
 $( document ).ready(function() {
-$("#pinpoint").click(function () {
-  $(".box").show("slow");
+
+$("#the-trip").click(function () {
+if ($("#about-box").is(":hidden")) {
+$("#about-box").slideDown("slow");
+} else {
+$("#about-box").slideUp("slow");
+}
 });
 
-$("#hidr").click(function () {
-  $("div").hide(1000);
+
+
+
+
+
+
+$("#pinpoint-5").click(function () {
+  $("#box-5").show("fade");
+});
+
+$(".close").click(function () {
+  $("#box-5").hide("fade");
+});
+
+
+
+
+
+$("#pinpoint-1").click(function () {
+  $("#box-1").show("fade");
+});
+
+$(".close").click(function () {
+  $("#box-1").hide("fade");
+});
+
+
+
+
+
+$("#pinpoint-2").click(function () {
+  $("#box-2").show("fade");
+});
+
+$(".close").click(function () {
+  $("#box-2").hide("fade");
+});
+
+
+
+
+$("#pinpoint-3").click(function () {
+  $("#box-3").show("fade");
+});
+
+$(".close-vertical").click(function () {
+  $("#box-3").hide("fade");
+});
+
+
+
+
+$("#pinpoint-4").click(function () {
+  $("#box-4").show("fade");
+});
+
+$(".close-vertical").click(function () {
+  $("#box-4").hide("fade");
+});
+
+
+
+
+$("#pinpoint-6").click(function () {
+  $("#box-6").show("fade");
+});
+
+$(".close-vertical").click(function () {
+  $("#box-6").hide("fade");
+});
+
+
+
+$("#pinpoint-7").click(function () {
+  $("#box-7").show("fade");
+});
+
+$(".close").click(function () {
+  $("#box-7").hide("fade");
+});
+
+
+
+
+$("#pinpoint-8").click(function () {
+  $("#box-8").show("fade");
+});
+
+$(".close-vertical").click(function () {
+  $("#box-8").hide("fade");
+});
+
+
+
+$("#pinpoint-9").click(function () {
+  $("#box-9").show("fade");
+});
+
+$(".close-vertical").click(function () {
+  $("#box-9").hide("fade");
+});
+
+
+
+$("#pinpoint-10").click(function () {
+  $("#box-10").show("fade");
+});
+
+$(".close").click(function () {
+  $("#box-10").hide("fade");
+});
+
+
+
+
+$("#pinpoint-11").click(function () {
+  $("#box-11").show("fade");
+});
+
+$(".close-vertical").click(function () {
+  $("#box-11").hide("fade");
+});
+
+
+
+$("#pinpoint-13").click(function () {
+  $("#box-13").show("fade");
+});
+
+$(".close").click(function () {
+  $("#box-13").hide("fade");
+});
+
+
+
+
+$("#pinpoint-15").click(function () {
+  $("#box-15").show("fade");
+});
+
+$(".close").click(function () {
+  $("#box-15").hide("fade");
+});
+
+
+$("#pinpoint-16").click(function () {
+  $("#box-16").show("fade");
+});
+
+$(".close").click(function () {
+  $("#box-16").hide("fade");
 });
 
 
